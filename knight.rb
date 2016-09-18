9108e897de1aceab46e430d0c9ea01096e431010
fan222 <sfwahahaha222@gmail.com>
2016-09-17 19:39:12 -0700

    re-write knight.rb

--- knight.rb
+++ knight.rb
@@ -1,67 +1,78 @@
-require_relative 'tree'
+require_relative "PolyTreeNode"
+require "byebug"
+DELTAS = [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
 
 class KnightPathFinder
-  DELTAS = [[-2, 1], [-1, 2], [1,2], [2,1], [2, -1], [1,-2], [-1, -2], [-2, -1]]
-  def self.valid_move(pos)
-    possible_moves = []
+  attr_reader :start_pos
+
+  def initialize(start_pos)
+    @start_pos = start_pos
+    @visited_positions = [@start_pos]
+    @root_node = nil
+    build_move_tree
+  end
+
+  def self.valid_moves(pos)
+    moves = []
     DELTAS.each do |increment|
-      new_move = [pos.first + increment.first, pos.last+increment.last]
-      possible_moves.push(new_move) if (0..7).include?(new_move.first) && (0..7).include?(new_move.last)
+      new_position= [pos[0]+increment[0],pos[1]+increment[1]]
+      if new_position.valid?
+        moves << new_position
+      end
     end
-    possible_moves
-  end
-
-  def initialize(pos)
-    @position = pos
-    @visited_positions = [pos]
-  end
-
-  def visited_positions
-    @visited_positions
+    moves
   end
 
   def new_move_positions(pos)
-    possible_moves = KnightPathFinder.valid_move(pos)
-    @visited_positions.each do |position|
-      possible_moves.delete(position)
+    possible_moves = KnightPathFinder.valid_moves(pos)
+    possible_moves.delete_if do |move|
+      # debugger
+      @visited_positions.include?(move)
     end
     @visited_positions += possible_moves
     possible_moves
   end
 
+
   def build_move_tree
-    very_root = PolyTreeNode.new(@position)
-    queue = [very_root]
-    tree = [very_root]
+    @root_node = PolyTreeNode.new(@start_pos)
+    queue = [@root_node]
     until queue.empty?
-      root=queue.shift
-      children_positions = self.new_move_positions(root.value)
-      children_positions.each do |children_position|
-        child = PolyTreeNode.new(children_position)
+      root = queue.shift
+      children_moves = self.new_move_positions(root.value)
+      children_moves.each do |move|
+        child = PolyTreeNode.new(move)
         root.add_child(child)
-        tree.push(child)
         queue.push(child)
       end
     end
-    tree
   end
 
   def find_path(end_pos)
-    end_obj=nil
-    tree = self.build_move_tree
-    tree.each do |obj|
-      end_obj = obj if obj.value == end_pos
-    end
-    trace_path_back(end_obj)
+    end_node = @root_node.bfs(end_pos)
+    trace_path_back(end_node).reverse.map(&:value)
   end
 
-  def trace_path_back(end_obj)
-    path = [end_obj]
-    positions =[end_obj.value]
-    until positions.first == @position
-      path.unshift(path.first.parent)
-      positions.unshift(path.first.value)
+  def trace_path_back(end_node)
+    nodes = []
+    current_node = end_node
+    until current_node.nil?
+      nodes << current_node
+      current_node = current_node.parent
     end
-    positions
+    nodes
   end
+
+end
+
+class Array
+  def valid?
+    x,y = self
+    (0..7).include?(x) && (0..7).include?(y)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  kpf = KnightPathFinder.new([0,0])
+  p kpf.find_path([7, 6])
 end
